e655a1bb48e9c6ab1aea3dd0f7bfc26f5971ba8c
Remi Kobylka <kobylka.remi@gmail.com>
2024-06-11 15:32:43 +0200

    pokedex3poke

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,28 @@
 function PokemonCard() {
 
+    const pokemon = pokemonList[1]
+
     return(
         <figure>
-            <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/383.png" />
-            <figcaption> Groudon </figcaption>
+            {pokemon.imgSrc ?(<img src={pokemon.imgSrc} />) : (<p>???</p>)}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 }
 export default PokemonCard
+
+const pokemonList = [
+    {
+      name: "Lugia",
+      imgSrc:
+        "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/249.png",
+    },
+    {
+      name: "Ramboum",
+    },
+    {
+      name: "Groudon",
+      imgSrc:"https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/383.png"
+
+    }
+  ];
