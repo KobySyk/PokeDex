5cec772ff99e2afcb081fe654075fa11e5e5cac6
Remi Kobylka <kobylka.remi@gmail.com>
2024-06-11 12:26:46 +0200

    groudonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+function PokemonCard() {
+
+    return(
+        <figure>
+            <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/383.png" />
+            <figcaption> Groudon </figcaption>
+        </figure>
+    )
+}
+export default PokemonCard
